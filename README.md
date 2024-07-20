5f46e099f623c61d89d4607286bcf13e0c8dd7a7
Carlos Medina <keystrike95@gmail.com>
2024-07-19 23:59:02 -0600

    Changes in background and pokemon card position

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
-Ejercicio B
+# Features:
 
-1. Consumir la API  donde param puede tomar el valor de "id" o "name" del pokemon.
-
-2. Crear una interfaz que dé al usuario la opción de hacer la búsqueda por id de manera aleatoria (Considerar un rango del 1 al 150) o por nombre. Ejemplos:
-Búsqueda por id: https://pokeapi.co/api/v2/pokemon/132
-Búsqueda por nombre: https://pokeapi.co/api/v2/pokemon/ditto
-
-3. Mostrar la ficha del pokémon con la información más representativa.Para la elaboración de la ficha puede hacer uso de bootstrap, materialize o cualquier otra librería externa, en su defecto elaborarla nativamente con HTML y CSS.
-
-4. Colocar un botón para que despliegue más información detallada del pokémon (puede ser en un modal o una nueva página).
-
-5. Colocar un botón para añadir el pokemon a una tabla, si el usuario así lo desea, se generará una fila dinámicamente por cada criatura añadida (Un máximo de 6). Sí el pokémon que se desea añadir ya existe en la tabla, éste  no se insertará.
+Search Pokemon by:
+-name
+-number
--- main.js
+++ main.js
@@ -1,41 +1,65 @@
-let numeroAleatorio = Math.floor(Math.random() * 150) + 1,
-  URL = `https://pokeapi.co/api/v2/pokemon/${numeroAleatorio}`,
-  pokemonList = '';
+let randomNumber = Math.floor(Math.random() * 150) + 1,
+  URL = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`,
+  pokemonList = ''
 
-const $searchBar = document.getElementById("pokemonSearchBar"),
-  $searchButton = document.getElementById("buttonSearch"),
-  $pokemonContainer = document.getElementById("pokemon__container");
-  
-document.addEventListener("click", e => {
-    
+const $searchBar = document.getElementById('pokemonSearchBar'),
+  $searchButton = document.getElementById('buttonSearch'),
+  $pokemonContainer = document.getElementById('pokemon__container')
+
+document.addEventListener('click', (e) => {
   if (e.target == $searchButton) {
-    let param = $searchBar.value;
-    console.log(param, typeof(param))
-      URL = `https://pokeapi.co/api/v2/pokemon/${param}`
-      getPokemon(URL)
-   }
-  })
+    let param = $searchBar.value
+    console.log(param, typeof param)
+    URL = `https://pokeapi.co/api/v2/pokemon/${param}`
+    getPokemon(URL)
+  }
+})
 
 async function getPokemon(URL) {
   try {
-   pokemonList = ''
-  const res = await fetch(URL)
-   const data = await res.json()
-   console.log(data)
+    pokemonList = ''
+    const res = await fetch(URL)
+    const data = await res.json()
+    console.log(data)
 
-   pokemonList += `
-    <div class="pokemon__container">
-      <h3 class="pokemon__name">${data.name}</h3>
-      <img src="${data.sprites.front_default}" alt="image of ${data.name}" class="pokemon__img">
-      <div class="pokemon__id">${data.id}</div>
+    if (data.id > 150 || data.id < 1) {
+      alert('El pokémon está fuera del rango especificado')
+      return
+    }
+
+    pokemonList += `
+      <div id="pokemon__container__basic-info">
+        <h3 class="pokemon__name">${data.name}</h3>
+        <span class="pokemon__id">Nº${data.id}</span>
+      </div>
+    <div id="pokemon__container__image">
+      <img
+      src="${data.sprites.front_default}"
+      alt="image of ${data.name}"
+      class="pokemon__img"
+    />
     </div>
-  `
-   
-   $pokemonContainer.innerHTML = pokemonList;
+    <div id="pokemon__container__options">
+      <button>Agregar</button>
+      <button>Info</button>
+    </div>
 
- } catch (error) {
-  console.log(error)
- }
+  `
+    $pokemonContainer.dataset.id = data.id
+    $pokemonContainer.innerHTML = pokemonList
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 getPokemon(URL)
+
+/*
+<div id="ventanaModal" class="modal">
+    <div class="contenido-modal">
+        <span class="cerrar">&times;</span>
+        <h2>Ventana modal</h2>
+        <p>Esto es el texto de la ventana</p>
+    </div>
+  </div>
+*/
